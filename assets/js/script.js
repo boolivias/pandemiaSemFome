924a526f7a88f48b33ddd9971b92ab03cda23388
boolivias <jean_wylmer@hotmail.com>
2020-07-30 13:45:20 -0300

    navbar working and responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,39 @@
 window.onload = function () {
+    //Navbar Animation
+    document.addEventListener("scroll", () => {
+        //Change type navbar
+        const nav = document.querySelector(".navbar");
+        if (window.scrollY) {
+            nav.classList.add("navbar-shrink");
+        }
+        else {
+            nav.classList.remove("navbar-shrink");
+        }
+
+        //Change item-active
+        const sections = document.getElementsByTagName("section");
+        for (const section of sections) {
+            var top = section.offsetTop;
+            var bottom = top + section.offsetHeight;
+            if (window.scrollY + (window.innerHeight / 4) >= top &&
+                window.scrollY + window.innerHeight <= bottom) {
+                document.getElementsByClassName("item-active")[0].classList.remove("item-active");
+                document.querySelector('[href="#' + section.id + '"]').classList.add("item-active");
+            }
+        }
+    })
+
+    // Open/close side menu in device mobile
+    document.querySelector(".open-menu").addEventListener("click", () => {
+        document.querySelector(".navbar").setAttribute("data-mobile-sideMenu", "show");
+
+    });
+    document.querySelector(".close-menu").addEventListener("click", () => {
+        document.querySelector(".navbar").setAttribute("data-mobile-sideMenu", "hidden");
+
+    });
+
+    //Form input animation
     var inputs = document.getElementsByClassName("form-control");
     for (const input of inputs) {
         input.addEventListener("focus", () => {
